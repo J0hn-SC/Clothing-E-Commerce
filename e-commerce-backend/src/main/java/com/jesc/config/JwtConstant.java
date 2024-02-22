91858885d89f518715b3002217fb46d9c4b02207
John-Sanchez-Chilo <jsanchezchi@unsa.edu.pe>
2024-02-21 21:31:26 -0500

    Init, CORS, User Class

---
+++ e-commerce-backend/src/main/java/com/jesc/config/JwtConstant.java
@@ -0,0 +1,6 @@
+package com.jesc.config;
+
+public class JwtConstant {
+	public static final String SECRET_KEY = "asdagsffanhpafabnjlbjlabfhabgfobaefna";
+	public static final String JWT_HEADER = "Authorization";
+}
