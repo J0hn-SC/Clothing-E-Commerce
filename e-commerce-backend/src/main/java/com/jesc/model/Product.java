3fbe48587dc292952ca3d6212766ca484a2c47bc
John-Sanchez-Chilo <jsanchezchi@unsa.edu.pe>
2024-02-21 21:41:27 -0500

    Rating class

---
+++ e-commerce-backend/src/main/java/com/jesc/model/Product.java
@@ -0,0 +1,20 @@
+package com.jesc.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Product {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	@Column(name = "title")
+	private String title;
+	
+	@Column(name = "description")
+	private String description;
+}
